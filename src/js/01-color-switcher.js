0d36bb82da99453a334a28dd88796f73a296dc28
zetdou <zyziupoz@gmail.com>
2024-04-13 12:16:42 +0200

    task-01
    first steps

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener("click", ev => {
+    const colorChange = getRandomHexColor();
+    document.body.style.backgroundColor = colorChange;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  //btnStart.setAttribute('disabled', '');
