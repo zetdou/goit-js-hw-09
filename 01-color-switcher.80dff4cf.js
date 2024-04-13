946be992768fd669da2d3485a88fb3231fa5950b
zetdou <149190114+zetdou@users.noreply.github.com>
2024-04-13 10:17:07 +0000

    Deploying to gh-pages from @ zetdou/goit-js-hw-09@0d36bb82da99453a334a28dd88796f73a296dc28 🚀

---
+++ 01-color-switcher.80dff4cf.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(t=>{const o=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=o}));
+//# sourceMappingURL=01-color-switcher.80dff4cf.js.map
