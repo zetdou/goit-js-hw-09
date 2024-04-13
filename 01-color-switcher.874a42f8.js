289d0acabb1611b287f866bd705b3bb75495463e
zetdou <149190114+zetdou@users.noreply.github.com>
2024-04-13 11:10:33 +0000

    Deploying to gh-pages from @ zetdou/goit-js-hw-09@fb8d9a52abcf2fabf8113e94bd4e456eccdbce77 🚀

---
+++ 01-color-switcher.874a42f8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled",""),n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.874a42f8.js.map
--- 01-color-switcher.c1f8ae66.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(t){var o="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=o}))}();
-//# sourceMappingURL=01-color-switcher.c1f8ae66.js.map
