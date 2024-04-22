766fe9e545919d1ad7e9d1505636265bac872654
zetdou <zyziupoz@gmail.com>
2024-04-22 18:56:53 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve({ position, delay });
   } else {
-    // Reject
+    return Promise.reject({ position, delay });
   }
 }
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", ev => {
+  ev.preventDefault();
+
+  const { elements } = ev.currentTarget;
+  const delay = Number(elements.delay.value);
+  const step = Number(elements.step.value);
+  const amount = Number(elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + i * step;
+
+    setTimeout(() => {
+      createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+    }, currentDelay);
+  }
+});
+
